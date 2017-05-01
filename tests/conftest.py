8a0d90c89c01df47d7a20faf65690a33201fdb33
snowman2 <alan.d.snow@usace.army.mil>
2017-05-01 10:17:22 -0500

    modified tests to use pytest fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+#
+#  conftest.py
+#  sloot
+#
+#  Author : Alan D Snow, 2017.
+#  License: BSD 3-Clause
+
+import itertools
+from numpy import array
+from numpy.testing import assert_almost_equal
+import pytest
+import os
+from osgeo import gdal
+from shutil import rmtree
+
+SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def compare_files(original, new, raster=False, precision=7):
+    """Compare the contents of two files"""
+    if raster:
+        ds_o = gdal.Open(original)
+        ds_n = gdal.Open(new)
+
+        # compare data
+        r_o = array(ds_o.ReadAsArray())
+        r_n = array(ds_n.ReadAsArray())
+        assert_almost_equal(r_o, r_n, decimal=precision)
+
+        # compare geotransform
+        assert_almost_equal(ds_o.GetGeoTransform(), ds_n.GetGeoTransform(),
+                            decimal=9)
+
+        # compare band counts
+        assert ds_o.RasterCount == ds_n.RasterCount
+        # compare nodata
+        for band_id in range(1, ds_o.RasterCount + 1):
+            assert (ds_o.GetRasterBand(band_id).GetNoDataValue()
+                    == ds_n.GetRasterBand(band_id).GetNoDataValue())
+
+    else:
+        with open(original) as fileO:
+            contents_o = fileO.read()
+            lines_o = contents_o.strip().split()
+
+        with open(new) as fileN:
+            contents_n = fileN.read()
+            lines_n = contents_n.strip().split()
+
+        for line_o, line_n in zip(lines_o, lines_n):
+            try:
+                val_o = float(line_o)
+                val_n = float(line_n)
+                assert_almost_equal(val_o, val_n)
+            except ValueError:
+                assert lines_o == lines_n
+
+
+class TestDirectories(object):
+    input = os.path.join(SCRIPT_DIR, 'input')
+    compare = os.path.join(SCRIPT_DIR, 'compare')
+    write = os.path.join(SCRIPT_DIR, 'output')
+
+    def clean(self):
+        """
+        Clean out test directory
+        """
+        os.chdir(self.write)
+
+        # Clear out directory
+        file_list = os.listdir(self.write)
+
+        for afile in file_list:
+            if not afile.endswith('.gitignore'):
+                path = os.path.join(self.write, afile)
+                if os.path.isdir(path):
+                    rmtree(path)
+                else:
+                    os.remove(path)
+
+
+@pytest.fixture(scope="module")
+def tgrid(request):
+    _td = TestDirectories()
+    _td.clean()
+
+    yield _td
+
+    _td.clean()
+
+
+
+
--- tests/test_rasterize_shapefile.py
+++ tests/test_rasterize_shapefile.py
@@ -9,197 +9,214 @@
 from glob import glob
 from os import path
 import os
-import unittest
+import pytest
 from shutil import copy
 
-from .template import TestGridTemplate
+from .conftest import compare_files
 
-from sloot.grid_tools import rasterize_shapefile
+from sloot.grid import rasterize_shapefile
 
-class TestResampleShapefile(TestGridTemplate):
-    def setUp(self):
+
+@pytest.fixture
+def get_wkt(request):
+    return ('PROJCS["WGS 84 / UTM zone 51N",GEOGCS["WGS 84",'
+            'DATUM["WGS_1984",SPHEROID["WGS 84",6378137,'
+            '298.257223563,AUTHORITY["EPSG","7030"]],'
+            'AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,'
+            'AUTHORITY["EPSG","8901"]],UNIT["degree",'
+            '0.01745329251994328,AUTHORITY["EPSG","9122"]],'
+            'AUTHORITY["EPSG","4326"]],UNIT["metre",1,'
+            'AUTHORITY["EPSG","9001"]],'
+            'PROJECTION["Transverse_Mercator"],'
+            'PARAMETER["latitude_of_origin",0],'
+            'PARAMETER["central_meridian",123],'
+            'PARAMETER["scale_factor",0.9996],'
+            'PARAMETER["false_easting",500000],'
+            'PARAMETER["false_northing",0],'
+            'AUTHORITY["EPSG","32651"],AXIS["Easting",EAST],'
+            'AXIS["Northing",NORTH]]')
+
+
+class MaskTest:
+    def __init__(self, tgrid):
+        tgrid.clean()
+
+        self.tgrid = tgrid
+
         # define global variables
-        self.wkt = ('PROJCS["WGS 84 / UTM zone 51N",GEOGCS["WGS 84",'
-                    'DATUM["WGS_1984",SPHEROID["WGS 84",6378137,'
-                    '298.257223563,AUTHORITY["EPSG","7030"]],'
-                    'AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,'
-                    'AUTHORITY["EPSG","8901"]],UNIT["degree",'
-                    '0.01745329251994328,AUTHORITY["EPSG","9122"]],'
-                    'AUTHORITY["EPSG","4326"]],UNIT["metre",1,'
-                    'AUTHORITY["EPSG","9001"]],'
-                    'PROJECTION["Transverse_Mercator"],'
-                    'PARAMETER["latitude_of_origin",0],'
-                    'PARAMETER["central_meridian",123],'
-                    'PARAMETER["scale_factor",0.9996],'
-                    'PARAMETER["false_easting",500000],'
-                    'PARAMETER["false_northing",0],'
-                    'AUTHORITY["EPSG","32651"],AXIS["Easting",EAST],'
-                    'AXIS["Northing",NORTH]]')
-
-        self.shapefile_path = path.join(self.writeDirectory,
+        self.shapefile_path = path.join(tgrid.write,
                                         'phillipines_5070115700.shp')
 
-        self.projected_shapefile = path.join(self.writeDirectory,
+        self.projected_shapefile = path.join(tgrid.write,
                                              'phillipines_5070115700_projected.shp')
-        self.compare_path = path.join(self.compareDirectory,
+        self.compare_path = path.join(tgrid.compare,
                                       'gdal_grid')
         # copy shapefile
-        shapefile_basename = path.join(self.inputDirectory,
+        shapefile_basename = path.join(tgrid.input,
                                        'gdal_grid',
                                        'phillipines_5070115700.*')
 
         for shapefile_part in glob(shapefile_basename):
             try:
                 copy(shapefile_part,
-                     path.join(self.writeDirectory, path.basename(shapefile_part)))
+                     path.join(tgrid.write, path.basename(shapefile_part)))
             except OSError:
                 pass
 
-    def _before_teardown(self):
-        '''
-        Method to execute at beginning of tearDown
-        '''
-        # make sure cleanup worked (fails on Windows)
-        if os.name != 'nt':
-            assert not path.exists(self.projected_shapefile)
-
-    def _compare_masks(self, mask_name):
-        '''
-        compare mask files
-        '''
-        new_mask_grid = path.join(self.writeDirectory, mask_name)
+    def compare_masks(self, mask_name):
+        """compare mask files"""
+        new_mask_grid = path.join(self.tgrid.write, mask_name)
         compare_msk_file = path.join(self.compare_path, mask_name)
-        self._compare_files(compare_msk_file, new_mask_grid, raster=True)
+        compare_files(compare_msk_file, new_mask_grid, raster=True)
 
-    def test_rasterize_num_cells(self):
-        '''
-        Tests rasterize_shapefile default using num cells
-        '''
-        mask_name = 'mask_50.msk'
-        new_mask_grid = path.join(self.writeDirectory, mask_name)
-        rasterize_shapefile(self.shapefile_path,
-                            new_mask_grid,
-                            x_num_cells=50,
-                            y_num_cells=50,
-                            )
-        # compare msk
-        self._compare_masks(mask_name)
 
-    def test_rasterize_num_cells_utm(self):
-        '''
-        Tests rasterize_shapefile default using num cells and utm
-        '''
-        mask_name = 'mask_50_utm.msk'
-        new_mask_grid = path.join(self.writeDirectory, mask_name)
-        rasterize_shapefile(self.shapefile_path,
-                            new_mask_grid,
-                            x_num_cells=50,
-                            y_num_cells=50,
-                            raster_nodata=0,
-                            convert_to_utm=True,
-                            )
-        # compare msk
-        self._compare_masks(mask_name)
+@pytest.fixture
+def prep(request, tgrid):
+    _mt = MaskTest(tgrid)
 
-    def test_rasterize_num_cells_wkt(self):
-        '''
-        Tests rasterize_shapefile default using num cells and wkt
-        '''
-        mask_name = 'mask_50_wkt.msk'
-        new_mask_grid = path.join(self.writeDirectory, mask_name)
-        rasterize_shapefile(self.shapefile_path,
-                            new_mask_grid,
-                            x_num_cells=50,
-                            y_num_cells=50,
-                            raster_nodata=0,
-                            raster_wkt_proj=self.wkt,
-                            )
-        # compare msk
-        self._compare_masks(mask_name)
+    # RUN TEST
+    yield _mt
 
-    def test_rasterize_num_cells_utm_ascii(self):
-        '''
-        Tests rasterize_shapefile default using num cells and utm to ascii
-        '''
-        mask_name = 'mask_50_utm_ascii.msk'
-        new_mask_grid = path.join(self.writeDirectory, mask_name)
-        gr = rasterize_shapefile(self.shapefile_path,
-                                 x_num_cells=50,
-                                 y_num_cells=50,
-                                 raster_nodata=0,
-                                 convert_to_utm=True,
-                                 as_gdal_grid=True,
-                                 )
-        gr.to_grass_ascii(new_mask_grid, print_nodata=False)
-        # compare msk
-        self._compare_masks(mask_name)
+    # BEFORE TEARDOWN
+    # make sure cleanup worked (fails on Windows)
+    if os.name != 'nt':
+        assert not path.exists(_mt.projected_shapefile)
 
-    def test_rasterize_num_cells_ascii(self):
-        '''
-        Tests rasterize_shapefile default using num cells to ascii
-        '''
-        mask_name = 'mask_50_ascii.msk'
-        new_mask_grid = path.join(self.writeDirectory, mask_name)
-        gr = rasterize_shapefile(self.shapefile_path,
-                                 x_num_cells=50,
-                                 y_num_cells=50,
-                                 raster_nodata=0,
-                                 as_gdal_grid=True,
-                                 )
-        gr.to_grass_ascii(new_mask_grid, print_nodata=False)
-        # compare msk
-        self._compare_masks(mask_name)
 
-    def test_rasterize_num_cells_wkt_ascii(self):
-        '''
-        Tests rasterize_shapefile default using num cells
-        '''
-        mask_name = 'mask_50_wkt_ascii.msk'
-        new_mask_grid = path.join(self.writeDirectory, mask_name)
-        gr = rasterize_shapefile(self.shapefile_path,
-                                 x_num_cells=50,
-                                 y_num_cells=50,
-                                 raster_nodata=0,
-                                 raster_wkt_proj=self.wkt,
-                                 as_gdal_grid=True,
-                                 )
-        gr.to_grass_ascii(new_mask_grid, print_nodata=False)
-        # compare msk
-        self._compare_masks(mask_name)
+def test_rasterize_num_cells(prep):
+    """
+    Tests rasterize_shapefile default using num cells
+    """
+    mask_name = 'mask_50.msk'
+    new_mask_grid = path.join(prep.tgrid.write, mask_name)
+    rasterize_shapefile(prep.shapefile_path,
+                        new_mask_grid,
+                        x_num_cells=50,
+                        y_num_cells=50,
+                        )
+    # compare msk
+    prep.compare_masks(mask_name)
 
-    def test_rasterize_cell_size_ascii(self):
-        '''
-        Tests rasterize_shapefile default using cell size to ascii
-        '''
-        mask_name = 'mask_cell_size_ascii.msk'
-        new_mask_grid = path.join(self.writeDirectory, mask_name)
-        gr = rasterize_shapefile(self.shapefile_path,
-                                 x_cell_size=0.01,
-                                 y_cell_size=0.01,
-                                 raster_nodata=0,
-                                 as_gdal_grid=True,
-                                 )
-        gr.to_grass_ascii(new_mask_grid, print_nodata=False)
-        # compare msk
-        self._compare_masks(mask_name)
 
-    def test_rasterize_cell_size_ascii_utm(self):
-        '''
-        Tests rasterize_shapefile using cell size to ascii in utm
-        '''
-        mask_name = 'mask_cell_size_ascii_utm.msk'
-        new_mask_grid = path.join(self.writeDirectory, mask_name)
-        gr = rasterize_shapefile(self.shapefile_path,
-                                 x_cell_size=1000,
-                                 y_cell_size=1000,
-                                 raster_nodata=0,
-                                 as_gdal_grid=True,
-                                 convert_to_utm=True,
-                                 )
-        gr.to_grass_ascii(new_mask_grid, print_nodata=False)
+def test_rasterize_num_cells_utm(prep):
+    """
+    Tests rasterize_shapefile default using num cells and utm
+    """
+    mask_name = 'mask_50_utm.msk'
+    new_mask_grid = path.join(prep.tgrid.write, mask_name)
+    rasterize_shapefile(prep.shapefile_path,
+                        new_mask_grid,
+                        x_num_cells=50,
+                        y_num_cells=50,
+                        raster_nodata=0,
+                        convert_to_utm=True,
+                        )
+    # compare msk
+    prep.compare_masks(mask_name)
 
-        # compare msk
-        self._compare_masks(mask_name)
 
-if __name__ == '__main__':
-    unittest.main()
+def test_rasterize_num_cells_wkt(prep, get_wkt):
+    """
+    Tests rasterize_shapefile default using num cells and wkt
+    """
+    mask_name = 'mask_50_wkt.msk'
+    new_mask_grid = path.join(prep.tgrid.write, mask_name)
+    rasterize_shapefile(prep.shapefile_path,
+                        new_mask_grid,
+                        x_num_cells=50,
+                        y_num_cells=50,
+                        raster_nodata=0,
+                        raster_wkt_proj=get_wkt,
+                        )
+    # compare msk
+    prep.compare_masks(mask_name)
+
+
+def test_rasterize_num_cells_utm_ascii(prep):
+    """
+    Tests rasterize_shapefile default using num cells and utm to ascii
+    """
+    mask_name = 'mask_50_utm_ascii.msk'
+    new_mask_grid = path.join(prep.tgrid.write, mask_name)
+    gr = rasterize_shapefile(prep.shapefile_path,
+                             x_num_cells=50,
+                             y_num_cells=50,
+                             raster_nodata=0,
+                             convert_to_utm=True,
+                             as_gdal_grid=True,
+                             )
+    gr.to_grass_ascii(new_mask_grid, print_nodata=False)
+    # compare msk
+    prep.compare_masks(mask_name)
+
+
+def test_rasterize_num_cells_ascii(prep):
+    """
+    Tests rasterize_shapefile default using num cells to ascii
+    """
+    mask_name = 'mask_50_ascii.msk'
+    new_mask_grid = path.join(prep.tgrid.write, mask_name)
+    gr = rasterize_shapefile(prep.shapefile_path,
+                             x_num_cells=50,
+                             y_num_cells=50,
+                             raster_nodata=0,
+                             as_gdal_grid=True,
+                             )
+    gr.to_grass_ascii(new_mask_grid, print_nodata=False)
+    # compare msk
+    prep.compare_masks(mask_name)
+
+
+def test_rasterize_num_cells_wkt_ascii(prep, get_wkt):
+    """
+    Tests rasterize_shapefile default using num cells
+    """
+    mask_name = 'mask_50_wkt_ascii.msk'
+    new_mask_grid = path.join(prep.tgrid.write, mask_name)
+    gr = rasterize_shapefile(prep.shapefile_path,
+                             x_num_cells=50,
+                             y_num_cells=50,
+                             raster_nodata=0,
+                             raster_wkt_proj=get_wkt,
+                             as_gdal_grid=True,
+                             )
+    gr.to_grass_ascii(new_mask_grid, print_nodata=False)
+    # compare msk
+    prep.compare_masks(mask_name)
+
+
+def test_rasterize_cell_size_ascii(prep):
+    """
+    Tests rasterize_shapefile default using cell size to ascii
+    """
+    mask_name = 'mask_cell_size_ascii.msk'
+    new_mask_grid = path.join(prep.tgrid.write, mask_name)
+    gr = rasterize_shapefile(prep.shapefile_path,
+                             x_cell_size=0.01,
+                             y_cell_size=0.01,
+                             raster_nodata=0,
+                             as_gdal_grid=True,
+                             )
+    gr.to_grass_ascii(new_mask_grid, print_nodata=False)
+    # compare msk
+    prep.compare_masks(mask_name)
+
+
+def test_rasterize_cell_size_ascii_utm(prep):
+    """
+    Tests rasterize_shapefile using cell size to ascii in utm
+    """
+    mask_name = 'mask_cell_size_ascii_utm.msk'
+    new_mask_grid = path.join(prep.tgrid.write, mask_name)
+    gr = rasterize_shapefile(prep.shapefile_path,
+                             x_cell_size=1000,
+                             y_cell_size=1000,
+                             raster_nodata=0,
+                             as_gdal_grid=True,
+                             convert_to_utm=True,
+                             )
+    gr.to_grass_ascii(new_mask_grid, print_nodata=False)
+
+    # compare msk
+    prep.compare_masks(mask_name)
+
