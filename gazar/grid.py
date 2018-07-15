488dbc5cf1c21264f4bc9e7242cff936ecdc6cb1
Alan D. Snow <alansnow21@gmail.com>
2018-07-14 20:44:16 -0500

    Update coords to use method from: https://github.com/pydata/xarray/pull/1712

--- gazar/grid.py
+++ gazar/grid.py
@@ -323,6 +323,7 @@ class GDALGrid(object):
     @property
     def coords(self):
         """Returns x and y coordinate arrays representing the grid.
+        Use method from: https://github.com/pydata/xarray/pull/1712
 
         Returns
         -------
@@ -331,13 +332,8 @@ class GDALGrid(object):
         x_coords: :func:`numpy.array`
             The X coordinate array.
         """
-        y_coords = np.zeros((self.y_size, self.x_size))
-        x_coords = np.zeros((self.y_size, self.x_size))
-        for xii, yii in itertools.product(
-                range(self.x_size), range(self.y_size)):
-            x_coords[yii, xii], y_coords[yii, xii] = \
-                self.pixel2coord(xii, yii)
-
+        x_coords, _ = (np.arange(self.x_size) + 0.5, np.zeros(self.x_size) + 0.5) * self.affine
+        _, y_coords = (np.zeros(self.y_size) + 0.5, np.arange(self.y_size) + 0.5) * self.affine
         return y_coords, x_coords
 
     @property
